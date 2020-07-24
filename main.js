0219a8afad836255c66daa95d2509b01369e0442
L.Walker <logantwalker95@gmail.com>
2020-07-23 20:53:36 -0400

    linked jquery in html

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-function recipeApiCall(){
-    $.ajax()
+function recipeApiCall(userInput) {
 
+    const apiKey = 'ca76d8942af543f39553ed0542c27990';
+
+    
+    const onReject = (errThrown) => {
+        console.log(errThrown);
+        console.log(errThrown.responseText);
+    }
+
+    $.ajax({
+        
+        url: "https://api.spoonacular.com/food/products/search?query="+ userInput + "&apiKey=" + apiKey,
+
+    }).then(function (response) {
+        console.log(response.products);
+    }, onReject);
+
+
+    
+    
 }
+
+recipeApiCall('lasagna');
