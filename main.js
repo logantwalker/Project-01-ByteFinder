6291120e223822c496097d58a7560cb5555522d0
L.Walker <logantwalker95@gmail.com>
2020-07-24 18:19:09 -0400

    comments added

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+//search for recipes 
 function recipeSearchCall(userInput) {
 
     const apiKey = 'ca76d8942af543f39553ed0542c27990';
 
-    
+    //reporting if api call throws error
     const onReject = (errThrown) => {
         console.log(errThrown);
         console.log(errThrown.responseText);
@@ -23,8 +24,11 @@ function recipeSearchCall(userInput) {
 
 }
 
+//searching here (input: string)
 recipeSearchCall('burrito');
 
+
+//get searched recipe by recipe.id value
 function getRecipeCall(id){
 
     const apiKey = 'ca76d8942af543f39553ed0542c27990';
@@ -42,38 +46,42 @@ function getRecipeCall(id){
 
         console.log(response);
 
+        //appending title and image for api response
         $('#testContainer').append(`<h1>${response.title}</h1>`);
         $('#testContainer').append('<div class="divider"></div>');
         $('#testContainer').append(`<img class="materialboxed center-align" style='width: 300px; margin-top:5%;' id='${response.id}'>`);
         $(`#${response.id}`).attr('src',`${response.image}`);
 
+        //creating div for recipe information
         let infoEl = `<div class='row valign-wrapper' id='${response.id}-info'></div>`;
-
         $('#testContainer').append(infoEl);
 
+        //rendering recipe information and summary
         $(`#${response.id}-info`).append(`<div style='margin-top:2%;' class='col'>Servings: ${response.servings}</div>`);
         $(`#${response.id}-info`).append(`<div style='margin-top:2%;' class='col'>Cook Time: ${response.readyInMinutes} min</div>`);
         $(`#${response.id}-info`).append(`<div style='margin-top:2%;' class='col'>Likes: ${response.aggregateLikes} <i class="material-icons">thumb_up</i></div>`);
         $(`#testContainer`).append(`<div class='col s7'>${response.summary}</div`)
 
+        //creating ingredients list
         $(`#testContainer`).append(`<div class='col s4' id='${response.id}-item-parent'><h4 style='margin-top:0; border-bottom:1px solid #c0c0c0'>Ingredients</h4></div>`);
         $(`#${response.id}-item-parent`).append(`<ul id='${response.id}-items'></ul>`);
 
+        //populating ingredients list function
         const listItems = (itemArr) => {
             itemArr.forEach(item => {
 
                 console.log(item.original);
-                let li = `<li style='padding:1%;'>- ${item.original}</li>`
+                let li = `<li style='padding-bottom:3%;'><i style=' vertical-align: bottom'class='material-icons'>add_circle</i> ${item.original}</li>`;
                 $(`#${response.id}-items`).append(li);
 
             })
 
         };
 
+        //making ingredients list
         listItems(response.extendedIngredients);
 
-
-
+        //instructions list
         let listParent = `<div class='col s6'><ol id='${response.id}-steps'>Instructions</ol></div>`;
         $('#testContainer').append(listParent);
 
